536140f79034d325a8f525a1f7a8045f223d1e45
Reese Ganaway <reeseganaway@gmail.com>
2022-01-07 15:32:46 -0500

    Added feat: remove columns

--- gridFunctions.js
+++ gridFunctions.js
@@ -1,5 +1,8 @@
 function addRows(){
     let rows=document.getElementsByClassName('row')
+    /*if(rows.length==0){
+        document.createElement
+    }*/
     let origRow=rows[0]
     let newRow=origRow.cloneNode(true)
     document.querySelector(".mt-5.content").appendChild(newRow)
@@ -21,4 +24,15 @@ function removeRows(){
     if (rows.length>0){
         rows[0].remove()
     }
+}
+
+function removeColumns(){
+    let rows=document.getElementsByClassName("row")
+    if(rows.length>0){
+    for(let i=0;i<rows.length;i++){
+    let columns=rows[i].getElementsByClassName('col-sm border border-dark box white')
+    if(columns.length>0){
+    columns[0].remove()}
+    }
+}
 }
